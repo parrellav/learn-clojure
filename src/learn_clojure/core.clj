3dc21c2edd72bd683084c82917656cffb22d4e46
Parrella, Vincent <Vincent_Parrella@cable.comcast.com>
2016-04-01 15:31:52 -0400

    more functions

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -40,5 +40,77 @@
   (if-not (empty? a-seq)
     (if (singleton? a-seq)
       (first a-seq)
-    (max (first a-seq)
-         (max-element (rest a-seq))))))
+      (max (first a-seq)
+           (max-element (rest a-seq))))))
+
+(defn seq-max [seq-1 seq-2]
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
+
+(defn longest-sequence [seq]
+  (if-not (empty? seq)
+    (seq-max (first seq) (longest-sequence (rest seq)))))
+
+(defn my-filter [pred? a-seq]
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
+
+(defn only-numbers? [coll]
+  (cond
+    (empty? coll)
+    true
+    (number? (first coll))
+    (only-numbers? (rest coll))
+    :else
+    false))
+
+(defn sequence-contains? [elem a-seq]
+  (cond
+    (empty? a-seq)
+    false
+    (= elem (first a-seq))
+    true
+    :else
+    (sequence-contains? elem (rest a-seq))))
+
+(defn my-take-while [pred? a-seq]
+  (cond
+    (empty? a-seq) a-seq
+    (pred? (first a-seq)) (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+    :else (list)))
+
+(defn my-drop-while [pred? a-seq]
+  (cond
+    (empty? a-seq) a-seq
+    (pred? (first a-seq)) (rest a-seq)
+    :else (my-drop-while pred? (rest a-seq))
+    ))
+
+(defn seq= [seq-1 seq-2]
+  (cond
+    (and (empty? seq-1) (empty? seq-2)) true
+    (= (first seq-1) (first seq-2)) (seq= (rest seq-1) (rest seq-2))
+    :else false
+    ))
+
+(defn my-map [f seq-1 seq-2]
+  (cond
+    (or (empty? seq-1) (empty? seq-2)) (list)
+    (and (first seq-1) (first seq-2)) (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))
+    ))
+
+(defn factorial [n]
+  (if (zero? n)
+    1
+    (* n (factorial (dec n)))))
+
+(defn power [n k]
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))
+    ))
+
